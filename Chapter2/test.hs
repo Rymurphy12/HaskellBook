0692770691b9edc88c6117c5194bd81ab0a48afd
Ryan Murphy <rymurphy12@gmail.com>
2018-04-11 14:30:08 -0500

    Completed more Chapter 2 exercises

--- Chapter2/test.hs
+++ Chapter2/test.hs
@@ -5,7 +5,7 @@ sayHello x =
 triple x = x * 3
 
 {- 
-Ch2 Exercise 1
+Ch2  Evaluation Exercise 1
 
 Functions declared in a File:
 
@@ -24,8 +24,34 @@ In GHCI 8+, the let is optional
 -}
 
 
---CH2 Exercise 2
+--CH2 Evaluation Exercise 2
 squaredTimes314 x = 3.14 * (x * x)
 
---Ch2 Exercise 3
+--CH2 Evaluation Exercise 3
 squaredTimesPi x = pi * (x * x)
+
+{-CH2 Parentheses and Association Exercise 1
+
+    a) 8 + 7 * 9 
+    b) (8 + 7) * 9 
+
+    Answer = Yes, in b) the parens cause 8 and 7 to be added together 
+             rather than 9 and 7 to be multiplied, thus chaning the
+             result.
+
+Exercise 2
+    a) perimeter x y = (x * 2) + (y * 2)
+    b) perimeter x y = x * 2 + y * 2
+
+    Answer = No difference. Multiplication has a higher precidence than 
+             addition and no addition was included in the parents of a).
+
+Exercise 3
+    a) f x = x / 2 + 9
+    b) f x = x / (2 + 9)
+
+    Answer = Yes, there is a differnece. Divison has a higher precidence
+             than addition and by wrapping it in parents, b) cause the
+             addition to be performed first. This changes the result.
+
+-}
